991b68c3b8f7e873920240e0a12697c8f81e93d5
ElTowa <ElTowa@NTPHAT>
2023-07-14 11:13:01 +0700

    Added Dependency Injection in WPF with Caliburn Micro
    Adds DI via Simple Container through Caliburn Micro

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -11,14 +11,58 @@ namespace TRMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        /// <summary>
+        /// Read this document
+        /// https://caliburnmicro.com/documentation/simple-container
+        /// https://caliburnmicro.com/documentation/Tutorials/WPF/SimpleContainer
+        /// https://caliburnmicro.com/documentation/configuration
+        /// </summary>
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
         }
 
-        protected override void OnStartup(object sender, StartupEventArgs e)
+       protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewForAsync<ShellViewModel>();
         }
+  
+        protected override void Configure()
+        {   
+            // Configures the container for the application.
+            
+            // Set the instance of the container
+            _container.Instance(_container);
+
+            // Register the WindowManager and EventAggregator as singletons
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            // Get all types in the assembly that are classes and end with "ViewModel"
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                // Register each ViewModel type per request
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+
     }
 }
