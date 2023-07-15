5ee88a4e45ae80f214650e0d7310d60b0b30f588
ElTowa <ElTowa@NTPHAT>
2023-07-15 17:35:01 +0700

    Get Add To Cart Working
    Adds the functionality to add items to the cart, get the subtotal and update quantities.

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -11,14 +11,16 @@ using TRMDesktopUI.Library.Models;
 namespace TRMDesktopUI.ViewModels
 {
     public class SalesViewModel:Screen
-    {
+    { 
         #region Private Fields
         IProductEndpoint _productEndpoint;
 
         private BindingList<ProductModel> _products;
-        private BindingList<string> _cart;
+        private BindingList<CartItemModel> _cart= new BindingList<CartItemModel>();
 
-        private int _itemQuantity;
+        private ProductModel _selectedProduct;
+
+        private int _itemQuantity=1;
         
         #endregion
 
@@ -48,7 +50,7 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
-        public BindingList<string> Cart
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -65,6 +67,47 @@ namespace TRMDesktopUI.ViewModels
             {
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+            
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+        public string SubTotal
+        {
+            get
+            {
+                decimal subTotal = 0;
+
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+
+                return subTotal.ToString("C");
+            }
+        }
+        public string Tax
+        {
+            get
+            {
+                return "$0.00";
+            }
+        }
+        public string Total
+        {
+            get
+            {
+                return "$0.00";
             }
         }
 
@@ -75,7 +118,12 @@ namespace TRMDesktopUI.ViewModels
                 bool output = false;
 
                 // TODO Make sure something is selected
-                // TODO Make sure there is an item quantity
+                // Make sure there is an item quantity and not null
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }   
+
                 return output;
             }
         }
@@ -107,11 +155,32 @@ namespace TRMDesktopUI.ViewModels
         #region Methods
         public void AddToCart()
         {
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
 
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                // update the item quantity. Can't use notify property so this is a workaround
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
         }
         public void RemoveFromCart()
         {
-
+            NotifyOfPropertyChange(() => SubTotal);
         }
         public void CheckOut()
         {
