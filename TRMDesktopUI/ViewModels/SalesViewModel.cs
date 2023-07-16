cd220ba7a990fb06306681c54e1c8f86e70485ec
ElTowa <ElTowa@NTPHAT>
2023-07-16 14:07:41 +0700

    Remove From Cart Functioning
    Adds remove from cart functionality. This functionality is reliant on Automapper for the models to properly update.

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -26,6 +26,7 @@ namespace TRMDesktopUI.ViewModels
         private BindingList<CartItemDisplayModel> _cart= new BindingList<CartItemDisplayModel>();
 
         private ProductDisplayModel _selectedProduct;
+        private CartItemDisplayModel _selectedCartItem;
 
         private int _itemQuantity=1;
         
@@ -94,7 +95,16 @@ namespace TRMDesktopUI.ViewModels
                 NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
-
+        public CartItemDisplayModel SelectedCartItem
+        {
+            get { return _selectedCartItem; }
+            set
+            {
+                _selectedCartItem = value;
+                NotifyOfPropertyChange(() => SelectedCartItem);
+                NotifyOfPropertyChange(() => CanRemoveFromCart);
+            }
+        }
         public string SubTotal
         {
             get
@@ -141,7 +151,10 @@ namespace TRMDesktopUI.ViewModels
             {
                 bool output = false;
 
-                // Make sure something is selected
+                if (SelectedCartItem != null && SelectedCartItem?.Product.QuantityInStock > 0)
+                {
+                    output = true;
+                }
                 return output;
 
             }
@@ -190,6 +203,16 @@ namespace TRMDesktopUI.ViewModels
         }
         public void RemoveFromCart()
         {
+            SelectedCartItem.Product.QuantityInStock += 1;
+
+            if (SelectedCartItem.QuantityInCart > 1)
+            {
+                SelectedCartItem.QuantityInCart -= 1;
+            }
+            else
+            {
+                Cart.Remove(SelectedCartItem);
+            }
             NotifyOfPropertyChange(() => SubTotal);
             NotifyOfPropertyChange(() => Tax);
             NotifyOfPropertyChange(() => Total);
