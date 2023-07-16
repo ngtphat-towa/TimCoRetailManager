1b4de7cfde990862e9072d55ab08715b708042e1
ElTowa <ElTowa@NTPHAT>
2023-07-16 10:44:52 +0700

    Update EF Database Name
    Updates the EF database name to be EFData.

---
+++ TRMDataManager/Migrations/Configuration.cs
@@ -0,0 +1,29 @@
+﻿namespace TRMDataManager.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TRMDataManager.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            // Set this into true
+            // Open the Package Manager Console
+            // Type: Enable-Migrations
+            // Then type: Update-Database -Verbose
+            // Open the API and create a new account manually
+            // Change the  User ID associated with this account in TRMData
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TRMDataManager.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
